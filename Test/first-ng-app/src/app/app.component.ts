8ec5fd55eff81c5e7cf9f3bd0df39950e24a68a5
ZornZorro <ZornZorro@gmx.de>
2025-05-27 17:25:19 +0200

    Video bei 36:00; Data Binding

--- Test/first-ng-app/src/app/app.component.ts
+++ Test/first-ng-app/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent,HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   template: `
     <app-header />
     <main>
